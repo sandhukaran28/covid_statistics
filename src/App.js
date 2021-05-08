ca6f1d342ab4e5ccdd8c1b48e8506f292213ac0a
adityapuri75 <adityapuri75@gmail.com>
2021-05-08 20:46:44 +0530

    Status Bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
+import Aditya from "./Aditya";
 import './App.css';
+import MainCell from "./components/MainCell";
 
 function App() {
   return (
-    <div>
-      
+    <div >
+      <Aditya />
     </div>
   );
 }
